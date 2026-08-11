df1cce67e9f9a7fe7514b707ddb8965e781800e9
kawer95 <1034765569@qq.com>
2026-08-11 21:26:15 +0800

    Bootstrap registry for hamster ball color test

--- extra/src/test/java/com/animania/extra/AnimaniaHamsterBallItemTest.java
+++ extra/src/test/java/com/animania/extra/AnimaniaHamsterBallItemTest.java
@@ -1,11 +1,20 @@
 package com.animania.extra;
 
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.SharedConstants;
+import net.minecraft.server.Bootstrap;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class AnimaniaHamsterBallItemTest {
+    @BeforeAll
+    static void bootstrapMinecraftRegistries() {
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
+    }
+
     @Test
     void legacyMetadataOrderIsPreserved() {
         assertEquals(DyeColor.BLACK, AnimaniaHamsterBallItem.legacyDye(0));
