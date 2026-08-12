75d97550ce106f19b48502fe789c5cfddc3278ee
Kotonoha-ayato <136154684+Kotonoha-ayato@users.noreply.github.com>
2026-08-12 21:06:01 +0800

    Update AnimaniaBlinkingLayerTest.java
    修复翻白眼

--- base/src/test/java/com/animania/client/render/AnimaniaBlinkingLayerTest.java
+++ base/src/test/java/com/animania/client/render/AnimaniaBlinkingLayerTest.java
@@ -24,4 +24,22 @@ class AnimaniaBlinkingLayerTest {
         assertEquals("animania_catsdogs:textures/entity/cats/blink_1_left.png", textures[0].toString());
         assertEquals("animania_catsdogs:textures/entity/cats/blink_1_right.png", textures[1].toString());
     }
+
+    @Test
+    void pigBlinkOverlayUsesTheLegacyBreedTintInsteadOfWhite() {
+        assertEquals(0xE07F7D, AnimaniaBlinkingLayer.colorsFor(
+                ResourceLocation.fromNamespaceAndPath("animania_farm", "sow_yorkshire"))[0]);
+        assertEquals(0x421006, AnimaniaBlinkingLayer.colorsFor(
+                ResourceLocation.fromNamespaceAndPath("animania_farm", "hog_duroc"))[0]);
+    }
+
+    @Test
+    void HolsteinAndFriesianKeepTheirTwoLegacyEyeColors() {
+        assertEquals(0x1C242B, AnimaniaBlinkingLayer.colorsFor(
+                ResourceLocation.fromNamespaceAndPath("animania_farm", "cow_holstein"))[0]);
+        assertEquals(0xDEDEDE, AnimaniaBlinkingLayer.colorsFor(
+                ResourceLocation.fromNamespaceAndPath("animania_farm", "cow_holstein"))[1]);
+        assertEquals(0x463930, AnimaniaBlinkingLayer.colorsFor(
+                ResourceLocation.fromNamespaceAndPath("animania_farm", "cow_friesian"))[0]);
+    }
 }
